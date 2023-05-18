900f8465a7669c571f5a7eb927e46cf1b1604310
angelicawillrich <angelica@willri.ch>
2023-05-18 16:59:50 +0200

    added validations for the bookings controller functions

--- src/controllers/booking.controller.ts
+++ src/controllers/booking.controller.ts
@@ -13,6 +13,10 @@ export async function createBooking (req: Request, res: Response, next: NextFunc
         throw new MissingRequiredParams()
       }
 
+      if (!isValidObjectId(data.userId) || !isValidObjectId(data.petSitterId)) {
+        throw new InvalidId()
+      }
+
       const updatedData = {...data, status: 'pending'}
 
       const result = await bookingService.createBooking(updatedData)
@@ -26,25 +30,34 @@ export async function createBooking (req: Request, res: Response, next: NextFunc
 
   export async function updateBookingStatus (req: Request, res: Response, next: NextFunction) {
     try {
-        const bookingId = req.body.bookingId;
-        const update = {
-            status: req.body.status,
-        };
-        if (!isValidObjectId(bookingId)) {
-          throw new InvalidId()
-        }
-    
-        const result = await bookingService.updateBookingStatus(bookingId, update)
-    
-        res.json({result})
-      } catch (err) {
-        console.error('ERROR:', err)
-        next(err);
+      if (!req.body.bookingId || !req.body.status) {
+        throw new MissingRequiredParams()
       }
+
+      if (!isValidObjectId(req.body.bookingId)) {
+        throw new InvalidId()
+      }
+      const bookingId = req.body.bookingId;
+      const update = {
+          status: req.body.status,
+      };
+        
+    
+      const result = await bookingService.updateBookingStatus(bookingId, update)
+  
+      res.json({result})
+    } catch (err) {
+      console.error('ERROR:', err)
+      next(err);
+    }
   }
 
   export async function filterBooking (req: Request, res: Response, next: NextFunction) {
     try {
+      if (!req.body.filter) {
+        throw new MissingRequiredParams()
+      }
+      
       const filter = Object.fromEntries(new URLSearchParams(req.params.filter))
       const result = await bookingService.filterBooking(filter)
   
